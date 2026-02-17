7b8809320f8ef614b5c59f8211c643fffe9791d4
d3652875 <yw.kim@gooddrs.com>
2026-02-17 15:33:02 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,9 @@ android {
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
-        }
+     buildFeatures {
+    viewBinding = true
+}
     }
 
     defaultConfig {
@@ -45,6 +47,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
